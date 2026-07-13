861b50513ea32d93c5c37b293c991e2f0b24be68
urbanmarader <urbanmarader@gmail.com>
2026-07-13 21:09:15 +0200

    Update voice.py

--- avatar/voice.py
+++ avatar/voice.py
@@ -1,1 +1,11 @@
+import pyttsx3
 
+def speak(text):
+    engine = pyttsx3.init()
+    engine.setProperty("rate", 150)
+    engine.say(text)
+    engine.runAndWait()
+
+
+if __name__ == "__main__":
+    speak("Привет! Я ваш искусственный интеллект аватар.")
